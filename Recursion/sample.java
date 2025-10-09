a63f958fd7f0614e31d6ed1d23076f480de059c1
Aditya Mishra <63736485+aditya403@users.noreply.github.com>
2025-10-09 23:09:54 +0530

    Update sample.java

--- Recursion/sample.java
+++ Recursion/sample.java
@@ -51,15 +51,132 @@ public class sample {
 
 
 
+def script = 
+[
+"object-group network MCAFEE-AV-INTERNAL-SERVERS",
+" network-object host 10.238.67.18",
+" network-object host 168.162.99.130",
+" network-object host 10.238.67.19",
+" network-object host 168.162.99.132",
+"object-group network FIS-FIREEYE-ENCASE-BIT9-MCAFEE",
+" group-object MCAFEE-AV-INTERNAL-SERVERS",
+"access-list Outside-IN extended permit object-group ENCASE-MCAFEE-PORTS object-group MCAFEE-AV-INTERNAL-SERVERS object-group AvantGard_Servers_PHL_Inside log ",
+"access-list Inside-273_10.88.89.0/24-IN extended permit object HTTPS object VLAN273-10.88.89.0-24 object-group MCAFEE-AV-INTERNAL-SERVERS log ",
+"access-list Inside-273_10.88.89.0/24-IN extended permit object TCP-8080 object VLAN273-10.88.89.0-24 object-group MCAFEE-AV-INTERNAL-SERVERS log ",
+"access-list AVGD-LABS-VLAN530-10.110.4.0/24-IN extended permit object HTTPS object AVGD-LABS-VLAN530-10.110.4.0-24 object-group MCAFEE-AV-INTERNAL-SERVERS log ",
+"access-list AVGD-LABS-VLAN530-10.110.4.0/24-IN extended permit object TCP-8080 object AVGD-LABS-VLAN530-10.110.4.0-24 object-group MCAFEE-AV-INTERNAL-SERVERS log ",
+"nat (Outside,any) source static MCAFEE-AV-INTERNAL-SERVERS MCAFEE-AV-INTERNAL-SERVERS destination static AvantGard_Servers_PHL_Inside AvantGard_Servers_PHL_Inside",
+"object-group network FIREEYE-INTERNAL-SERVERS",
+" network-object host 168.162.99.132"
+
+]
+def objectGroups = [:]
+def parentObjects = [:]
+def acls = []
+def nats = []
+
+def current = []
+def currentName = null
+def isParent = false
+script.each{line->
+	line = line.trim()
+
+	if(line.startsWith("object-group")){
+		if(currentName){
+			if(isParent)
+				parentObjects[currentName] = current
+			else
+				objectGroups[currentName] = current
+		}
+		current = [line]
+		currentName = line.split("\\s+")[2] // Get the group name
+		isParent = false
+	}else if(line.startsWith("group-object")){
+		current << line
+		isParent = true
+	}else if(line.startsWith("access-list")){
+		current << line
+	}else{
+		if(current){
+			if(isParent)
+				parentObjects[currentName] = current
+			else
+				objectGroups[currentName] = current
+			current = []
+			currentName = null
+		}
+		if(line.startsWith("access-list")) acls << line
+		else if(line.startsWith("nat")) nats << line
+	}
+}
+// handle last block
+if(currentName){
+if(isParent)
+	parentObjects[currentName] = current
+else
+	objectGroups[currentName] = current
+}
+
+// --- Now detect dependencies ---
+def deletedGroups = [] as Set
+def parentRefMap = [:] //child -> Parent
+def aclMap = [:].withDefault{[]}
+def natMap = [:].withDefault{[]}
+
+parentObjects.each{pname, plines ->
+plines.each{l->
+	if(l.startsWith("group-object")){
+		def child = l.split("\\s+")[1]
+		parentRefMap[child] = pname
+	}
+}
+}
+acls.each{l->
+def matches = (l =~ /object-group\s+(\S+)/)
+matches.each{m->
+	aclMap[m[1]] << l
+}
+}
+
+nats.each{l->
+def matches = (l =~ /static\s+(\S+)/)
+matches.each{m->
+	natMap[m[1]] << l
+}
+}
+
+// Find fully deleted groups
+objectGroups.keySet().each{name->
+if(parentRefMap[name] || aclMap[name] || natMap[name]){
+	deletedGroups << name
+}
+}
+
+print("\n======== PRE FINAL BACKOUT ========\n")
+print(objectGroups)
+print("\n======== PRE FINAL BACKOUT ========\n")
+
+// --- Build Output ---
+def output = []
+
+objectGroups.each{name, block ->
+if(!deletedGroups.contains(name)){
+	output += block
+}
+}
 
 
+objectGroups.each{name->
+if(objectGroups[name]) output += objectGroups[name]
+if(parentRefMap[name] && parentObjects[parentRefMap[name]])
+	output += parentObjects[parentRefMap[name]]
+if(aclMap[name]) output += aclMap[name]
+if(natMap[name]) output += natMap[name]
+}
 
+def preBackoutScript = output.join("\n")
 
+print("\n======== FINAL BACKOUT ========\n")
+print(preBackoutScript)
+print("\n======== FINAL BACKOUT ========\n")
 
-======== PRE FINAL BACKOUT ========
-[MCAFEE-AV-INTERNAL-SERVERS:[object-group network MCAFEE-AV-INTERNAL-SERVERS], FIS-FIREEYE-ENCASE-BIT9-MCAFEE:[object-group network FIS-FIREEYE-ENCASE-BIT9-MCAFEE], null:[access-list Outside-IN extended permit object-group ENCASE-MCAFEE-PORTS object-group MCAFEE-AV-INTERNAL-SERVERS object-group AvantGard_Servers_PHL_Inside log, access-list Inside-273_10.88.89.0/24-IN extended permit object HTTPS object VLAN273-10.88.89.0-24 object-group MCAFEE-AV-INTERNAL-SERVERS log, access-list Inside-273_10.88.89.0/24-IN extended permit object TCP-8080 object VLAN273-10.88.89.0-24 object-group MCAFEE-AV-INTERNAL-SERVERS log, access-list AVGD-LABS-VLAN530-10.110.4.0/24-IN extended permit object HTTPS object AVGD-LABS-VLAN530-10.110.4.0-24 object-group MCAFEE-AV-INTERNAL-SERVERS log, access-list AVGD-LABS-VLAN530-10.110.4.0/24-IN extended permit object TCP-8080 object AVGD-LABS-VLAN530-10.110.4.0-24 object-group MCAFEE-AV-INTERNAL-SERVERS log], FIREEYE-INTERNAL-SERVERS:[object-group network FIREEYE-INTERNAL-SERVERS]]
-======== PRE FINAL BACKOUT ========
-
-======== FINAL BACKOUT ========
-
-======== FINAL BACKOUT ========
